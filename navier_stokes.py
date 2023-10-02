fix pressure gradient

navier_stokes.py
@@ -34,21 +34,11 @@ class NavierStokesVelVort(Equation):
         domain = Domain(shape, (True, False, True))
         super().__init__(domain, *fields)
         self.Re = params["Re"]
-        # vort = self.fields["vort"][0]
-        # vort.update_boundary_conditions()
+        self.flow_rate = self.get_flow_rate()
 
     @classmethod
     def FromVelocityField(cls, shape, velocity_field, Re=1.8e2):
         domain = Domain(shape, (True, False, True))
-        # vort = velocity_field.curl()
-        # for i in range(3):
-        #     vort[i].name = "vort_" + str(i)
-
-        # hel = velocity_field.cross_product(vort)
-        # for i in range(3):
-        #     hel[i].name = "hel_" + str(i)
-
-        # return cls(domain, velocity_field, vort, hel, Re=Re)
         velocity_field_hat = velocity_field.hat()
         velocity_field_hat.name = "velocity_hat"
         return cls(shape, velocity_field_hat, Re=Re)
@@ -138,12 +128,10 @@ class NavierStokesVelVort(Equation):
 
 
         # flow rate per unit thickness
-        flow_rate = self.get_flow_rate()
-        dPdx = - flow_rate * 3/2 / Re # TODO does this only hold for quadratic profiles?
-        # dPdx = - len(self.domain.grid[0]) * 2 / Re # should yield u_max=1; TODO: why does Nx (Ny?? Nz??) enter here?
-        # dPdx = - 2 / Re # should yield u_max=1
-        # dPdx = - 1
-        # dPdx = 0
+        Nx = len(self.domain.grid[0])
+        Nz = len(self.domain.grid[2])
+        # TODO how to generalize this to the turbulent case? Why is the number of grid points important?
+        dPdx = - (Nx * Nz)**(1/2) * self.flow_rate * 3/2 / Re
         dPdz = 0 # spanwise pressure gradient should be negligble
         D2 = self.get_cheb_mat_2_homogeneous_dirichlet()
         L_NS = 1 / Re * jnp.block([[D2, Z], [Z, D2]])

test.py
@@ -249,7 +249,7 @@ def test_fourier_1D():
 
 def test_fourier_2D():
     Nx = 24
-    Ny = Nx
+    Ny = Nx + 4
     domain = Domain((Nx,Ny), (True,True))
 
     u_fn = lambda X: jnp.cos(X[0]) * jnp.cos(X[1])
@@ -323,8 +323,8 @@ def test_fourier_2D():
 
 def test_fourier_simple_3D():
     Nx = 24
-    Ny = Nx
-    Nz = Nx
+    Ny = Nx + 4
+    Nz = Nx - 4
     domain = Domain((Nx,Ny,Nz), (True,False,True))
 
     # u_fn = lambda X: jnp.cos(X[0]) * jnp.cos(X[2]) * jnp.cos(X[1] * jnp.pi/2)
@@ -440,22 +440,26 @@ def test_poisson_slices():
 
 
 def test_navier_stokes_laminar():
-    Nx = 20
+    Nx = 22
     Ny = 24
     Nz = 18
 
-    Re = 1.8e1
+    Re = 1.8e0
 
     domain = Domain((Nx, Ny, Nz), (True, False, True))
 
     vel_x_fn = (
         lambda X: jnp.pi/3 * jnp.cos(X[1] * jnp.pi / 2 + 0.0 * X[0] * X[2])
     )
+    # add small pertubation in y and z to see if it decays
+    # TODO make sure this is div free
     vel_y_fn = (
-        lambda X: 0.0 * X[0] * X[1] * X[2]
+        # lambda X: 0.0 * X[0] * X[1] * X[2]
+        lambda X: 0.1 *  jnp.pi/3 * jnp.cos(X[1] * jnp.pi / 2 + 0.0 * X[0] * X[2])
     )
     vel_z_fn = (
-        lambda X: 0.0 * X[0] * X[1] * X[2]
+        # lambda X: 0.0 * X[0] * X[1] * X[2]
+        lambda X: 0.1 * jnp.pi/3 * jnp.cos(X[1] * jnp.pi / 2 + 0.0 * X[0] * X[2])
     )
     # vel_x_fn = (
     #     lambda X: 0.1 * jnp.cos(X[0])**2 * jnp.cos(X[2]+1.0) * jnp.cos(X[1] * jnp.pi / 2)
@@ -477,7 +481,7 @@ def test_navier_stokes_laminar():
     vel_x_ana = Field.FromFunc(domain, vel_x_fn_ana, name="vel_x_ana")
 
     nse = NavierStokesVelVort.FromVelocityField((Nx, Ny, Nz), vel, Re)
-    number_of_steps = 50
+    number_of_steps = 10
     flow_rate_0 = nse.get_flow_rate()
     for i in range(number_of_steps):
         print("Time Step " + str(i+1) + " of " + str(number_of_steps))
@@ -494,10 +498,10 @@ def test_navier_stokes_laminar():
         print("flow_rate_0: " + str(flow_rate_0))
         print("flow_rate: " + str(nse.get_flow_rate()))
     tol = 1e-7
-    print(abs(vel[0] - vel_x_ana[0]))
+    print(abs(vel[0] - vel_x_ana))
     print(abs(vel[1]))
     print(abs(vel[2]))
-    assert abs(vel[0] - vel_x_ana[0]) < tol
+    assert abs(vel[0] - vel_x_ana) < tol
     assert abs(vel[1]) < tol
     assert abs(vel[2]) < tol
 