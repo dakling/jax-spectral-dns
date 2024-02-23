8905591fb3d5716aac6a25ff6c508abe50ec993c
Dario <dario.klingenberg@web.de>
2024-02-23 13:43:32 +0000

    rename

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ jax.config.update("jax_enable_x64", True)
 import os
 import multiprocessing
 
-# max_cpus = 1
-# max_cpus = 6
-max_cpus = 1e10
+# max_devices = 1
+# max_devices = 6
+max_devices = 1e10
 os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count={}".format(
-    min(multiprocessing.cpu_count(), max_cpus)
+    min(multiprocessing.cpu_count(), max_devices)
 )
-print(jax.devices())
-print(jax.device_count())
 
 # from importlib import reload
 import sys
