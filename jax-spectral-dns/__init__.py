43681c3b01aa106852d2145e505d9fed41206314
Dario <dario.klingenberg@web.de>
2024-03-26 14:31:17 +0000

    add setup and init files

---
+++ jax-spectral-dns/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+jax-spectral-dns.
+
+A spectral DNS solver supporting automatic differentiation
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Dario Klingenberg'
+__credits__ = 'Dario Klingenberg'
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='jax-spectral-dns',
+    version='0.1.0',
+    description='A spectral DNS solver supporting automatic differentiation',
+    url='https://gitlab.com/dakling/jax-optim',
+    author='Dario Klingenberg',
+    author_email='dsk34@cam.ac.uk',
+    license='AGPL3',
+    packages=['jax-spectral-dns'],
+    install_requires=['jax',
+                      'numpy',
+                      'scipy',
+                      'matplotlib',
+                      'jaxopt',
+                      'optax',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GPL License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
