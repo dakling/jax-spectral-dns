dccb8703a5989c15d2e7c93d0949a95788098efc
Dario <dario.klingenberg@web.de>
2024-04-10 15:09:03 +0100

    fix

--- jax_spectral_dns/examples.py
+++ jax_spectral_dns/examples.py
@@ -2485,7 +2485,7 @@ def run_ld_2020(
         vel_hat = nse.get_field("velocity_hat", i)
         vel = vel_hat.no_hat()
 
-        vel_base_hat = nse.get_field("velocity_baser_hat", i)
+        vel_base_hat = nse.get_field("velocity_base_hat", i)
         vel_base = vel_base_hat.no_hat()
 
         vort = vel.curl()
