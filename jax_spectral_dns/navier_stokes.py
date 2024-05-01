9d36b1dae54c66efae81d88a9e79b7a0aed64537
Dario <dario.klingenberg@web.de>
2024-05-01 15:13:27 +0100

    parallelize over three dimensions (WIP)

--- jax_spectral_dns/navier_stokes.py
+++ jax_spectral_dns/navier_stokes.py
@@ -1024,7 +1024,17 @@ class NavierStokesVelVort(Equation):
             number_of_factors = len(factors)  # should always be divisible by 2
             return factors[number_of_factors // 2]
 
-        u0 = self.get_latest_field("velocity_hat").get_data()
+        from jax.sharding import Mesh
+        from jax.sharding import PartitionSpec
+        from jax.sharding import NamedSharding
+        from jax.experimental import mesh_utils
+
+        P = jax.sharding.PartitionSpec
+        n = jax.local_device_count()
+        devices = mesh_utils.create_device_mesh((n,))
+        mesh = jax.sharding.Mesh(devices, ("x",))
+        sharding = jax.sharding.NamedSharding(mesh, P("x"))  # type: ignore[no-untyped-call]
+        u0 = jax.device_put(self.get_latest_field("velocity_hat").get_data(), sharding)
         ts = jnp.arange(0, self.end_time, self.get_dt())
         number_of_time_steps = len(ts)
 
