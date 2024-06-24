93749739b4ed525f4c643382436ef03071b74b69
Dario <dario.klingenberg@web.de>
2024-06-24 11:24:03 +0100

    fix

--- jax_spectral_dns/main.py
+++ jax_spectral_dns/main.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
             func_name = sys.argv[1]
             try:
                 args = get_args_from_yaml_file()
-                globals()[func_name](args)
+                globals()[func_name](**args)
             except FileNotFoundError:
                 print_verb(
                     "WARNING: file simulation_settings.yml not found. Reading arguments from command line, which is discouraged and might not work."
