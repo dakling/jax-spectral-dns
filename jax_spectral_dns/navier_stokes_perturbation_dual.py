4f98415e07b310d2607a5f2795465997f3e7f05c
Dario <dario.klingenberg@web.de>
2024-06-26 14:07:29 +0100

    decrease tol further

--- jax_spectral_dns/navier_stokes_perturbation_dual.py
+++ jax_spectral_dns/navier_stokes_perturbation_dual.py
@@ -662,7 +662,7 @@ class NavierStokesVelVortPerturbationDual(NavierStokesVelVortPerturbation):
         print_verb("optimising lambda...", verbosity_level=2)
         i = 0
         max_iter = 100
-        tol = 1e-15  # can be fairly high as we normalize the result anyway
+        tol = 1e-25  # can be fairly high as we normalize the result anyway
         while abs(get_new_energy_0(lam) - e_0) / e_0 > tol and i < max_iter:
             lam += -(get_new_energy_0(lam) - e_0) / jax.grad(get_new_energy_0)(lam)
             i += 1
@@ -708,7 +708,7 @@ class NavierStokesVelVortPerturbationDual(NavierStokesVelVortPerturbation):
         print_verb("optimising lambda...")
         i = 0
         max_iter = 100
-        tol = 1e-15  # can be fairly high as we normalize the result anyway
+        tol = 1e-25  # can be fairly high as we normalize the result anyway
         while abs(get_new_energy_0(lam) - e_0) / e_0 > tol and i < max_iter:
             lam += -(get_new_energy_0(lam) - e_0) / jax.grad(get_new_energy_0)(lam)
             i += 1
