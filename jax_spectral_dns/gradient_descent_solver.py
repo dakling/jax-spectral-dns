94516027f7b2ebe369fd01cf4f1b1bbad71171a0
Dario <dario.klingenberg@web.de>
2024-06-26 13:14:51 +0100

    debug

--- jax_spectral_dns/gradient_descent_solver.py
+++ jax_spectral_dns/gradient_descent_solver.py
@@ -96,6 +96,9 @@ class GradientDescentSolver(ABC):
 
     def optimise(self) -> None:
         self.initialise(self.number_of_steps >= 0)
+        print_verb("hello1", self.value)
+        print_verb("hello2", self.old_value)
+        self.update_done(self.i)
         while not self.done:
             self.update()
             self.post_process_iteration()
