a845576683cb25a89d232212f11dcc797969cb05
Dario <dario.klingenberg@web.de>
2024-06-26 14:09:44 +0100

    fix

--- jax_spectral_dns/gradient_descent_solver.py
+++ jax_spectral_dns/gradient_descent_solver.py
@@ -367,7 +367,7 @@ class ConjugateGradientDescentSolver(GradientDescentSolver):
         else:
             self.grad, _ = self.dual_problem.get_projected_grad(self.step_size)
 
-        self.current_guess = self.current_guess + self.grad
+        self.current_guess = self.current_guess + self.step_size * self.grad
         self.current_guess = self.normalize_field(self.current_guess)
 
         if Equation.verbosity_level >= 3:
