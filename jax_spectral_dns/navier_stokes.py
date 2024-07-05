998ac848edb219aa89c97898cfa4058a70c78ccc
Dario <dario.klingenberg@web.de>
2024-07-05 12:51:54 +0100

    should be working now

--- jax_spectral_dns/navier_stokes.py
+++ jax_spectral_dns/navier_stokes.py
@@ -1553,69 +1553,76 @@ class NavierStokesVelVort(Equation):
                 ]
             )
 
-            # if step == number_of_rk_steps - 1:
-            #     if self.constant_mass_flux:
+            if step == number_of_rk_steps - 1:
+                if self.constant_mass_flux:
 
-            #         current_flow_rate = self.get_flow_rate(vel_new_hat_field)
-            #         flow_rate_diff = current_flow_rate - self.flow_rate
-            #         # most numerically accurate
-            #         velocity_correction = jnp.array(
-            #             [
-            #                 (
-            #                     jnp.ones(
-            #                         self.get_physical_domain().get_shape_aliasing()
-            #                     )
-            #                     * (-1 * flow_rate_diff * 0.5)
-            #                     if i == 0
-            #                     else jnp.zeros(
-            #                         self.get_physical_domain().get_shape_aliasing()
-            #                     )
-            #                 )
-            #                 for i in self.all_dimensions()
-            #             ]
-            #         )
-            #         vel_new_hat_field = jnp.array(
-            #             [
-            #                 self.get_physical_domain().field_hat(
-            #                     self.get_domain().field_no_hat(vel_new_hat_field[i])
-            #                     + velocity_correction[i]
-            #                 )
-            #                 for i in self.all_dimensions()
-            #             ]
-            #         )
-            #         # most efficient but numerical errors seem to be problematic -> TODO
-            #         # velocity_correction_hat = jnp.array(
-            #         #     [
-            #         #         (
-            #         #             self.get_physical_domain().field_hat(jnp.ones(self.get_physical_domain().get_shape()) * (-1 * flow_rate_diff * 0.5))
-            #         #             if i == 0
-            #         #             else jnp.zeros(self.get_physical_domain().get_shape())
-            #         #         )
-            #         #         for i in self.all_dimensions()
-            #         #     ]
-            #         # )
-            #         # vel_new_hat_field = vel_new_hat_field + velocity_correction_hat
-            #         # joe's version
-            #         # vel_new_hat_field = jnp.array(
-            #         #     [
-            #         #         (
-            #         #             FourierField(
-            #         #                 self.get_physical_domain(),
-            #         #                 vel_new_hat_field[i, ...],
-            #         #             ).no_hat()
-            #         #             - flow_rate_diff * 0.5 * (i == 0)
-            #         #         ).hat()[...]
-            #         #         for i in self.all_dimensions()
-            #         #     ]
-            #         # )
+                    current_flow_rate = self.get_flow_rate(vel_new_hat_field)
+                    flow_rate_diff = current_flow_rate - self.flow_rate
+                    # most numerically accurate
+                    # velocity_correction = jnp.array(
+                    #     [
+                    #         (
+                    #             jnp.ones(
+                    #                 self.get_physical_domain().get_shape_aliasing()
+                    #             )
+                    #             * (-1 * flow_rate_diff * 0.5)
+                    #             if i == 0
+                    #             else jnp.zeros(
+                    #                 self.get_physical_domain().get_shape_aliasing()
+                    #             )
+                    #         )
+                    #         for i in self.all_dimensions()
+                    #     ]
+                    # )
+                    vel_new_hat_field = jnp.array(
+                        [
+                            (
+                                self.get_physical_domain().field_hat(
+                                    self.get_domain().field_no_hat(vel_new_hat_field[i])
+                                    + jnp.ones(
+                                        self.get_physical_domain().get_shape_aliasing()
+                                    )
+                                    * (-1 * flow_rate_diff * 0.5)
+                                )
+                                if i == 0
+                                else jnp.zeros(self.get_physical_domain().get_shape())
+                            )
+                            for i in self.all_dimensions()
+                        ]
+                    )
+                    # most efficient but numerical errors seem to be problematic -> TODO
+                    # velocity_correction_hat = jnp.array(
+                    #     [
+                    #         (
+                    #             self.get_physical_domain().field_hat(jnp.ones(self.get_physical_domain().get_shape()) * (-1 * flow_rate_diff * 0.5))
+                    #             if i == 0
+                    #             else jnp.zeros(self.get_physical_domain().get_shape())
+                    #         )
+                    #         for i in self.all_dimensions()
+                    #     ]
+                    # )
+                    # vel_new_hat_field = vel_new_hat_field + velocity_correction_hat
+                    # joe's version
+                    # vel_new_hat_field = jnp.array(
+                    #     [
+                    #         (
+                    #             FourierField(
+                    #                 self.get_physical_domain(),
+                    #                 vel_new_hat_field[i, ...],
+                    #             ).no_hat()
+                    #             - flow_rate_diff * 0.5 * (i == 0)
+                    #         ).hat()[...]
+                    #         for i in self.all_dimensions()
+                    #     ]
+                    # )
 
-            #         dpdx = PhysicalField.FromFunc(
-            #             self.get_physical_domain(),
-            #             lambda X: dPdx + 0.0 * X[0] * X[1] * X[2],
-            #         ).hat()
-            #     else:
-            #         if Equation.verbosity_level >= 3:
-            #             self.dPdx = self.update_pressure_gradient(vel_new_hat_field)
+                    dpdx = PhysicalField.FromFunc(
+                        self.get_physical_domain(),
+                        lambda X: dPdx + 0.0 * X[0] * X[1] * X[2],
+                    ).hat()
+                else:
+                    if Equation.verbosity_level >= 3:
+                        self.dPdx = self.update_pressure_gradient(vel_new_hat_field)
 
             vel_hat_data = vel_new_hat_field
 
