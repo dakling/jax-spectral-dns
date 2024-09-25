b1a8f79fb0c1bd94e8b345d003c70160a3f98021
Dario <dario.klingenberg@web.de>
2024-09-25 09:32:16 +0100

    introduce max_iter for linesearch

--- jax_spectral_dns/gradient_descent_solver.py
+++ jax_spectral_dns/gradient_descent_solver.py
@@ -484,7 +484,7 @@ class ConjugateGradientDescentSolver(GradientDescentSolver):
         self.old_value: Optional["float"] = None
         self.old_grad: Optional["jnp_array"] = None
 
-    def get_step_size_ls(self, old_value: "float") -> "float":
+    def get_step_size_ls(self, old_value: "float", max_iter_ls: "int" = 20) -> "float":
 
         step_size = self.step_size
         print_verb("performing line search, step size", step_size)
@@ -529,7 +529,7 @@ class ConjugateGradientDescentSolver(GradientDescentSolver):
         print_verb("step_size * t", step_size * t)
         if cond:
             print_verb("wolfe conditions satisfied, trying to increase the step size")
-            while new_value - old_value > step_size * t:
+            while new_value - old_value > step_size * t and j < max_iter_ls:
                 step_size /= tau
                 print_verb("line search iteration", j, "step size", step_size)
 
@@ -576,7 +576,7 @@ class ConjugateGradientDescentSolver(GradientDescentSolver):
             print_verb(
                 "wolfe conditions not satisfied, trying to decrease the step size"
             )
-            while new_value - old_value < step_size * t:
+            while new_value - old_value < step_size * t and j < max_iter_ls:
                 step_size *= tau
 
                 print_verb("line search iteration", j, "step size", step_size)
