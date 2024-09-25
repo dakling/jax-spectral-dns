4b42eae3d75eeb4476a35cac920da1ee5034dbc1
Dario <dario.klingenberg@web.de>
2024-09-25 16:32:03 +0100

    avoid double update

--- jax_spectral_dns/gradient_descent_solver.py
+++ jax_spectral_dns/gradient_descent_solver.py
@@ -695,6 +695,7 @@ class ConjugateGradientDescentSolver(GradientDescentSolver):
         if (not self.use_linesearch) or (self.value is None):
             gain = self.dual_problem.get_objective_fun()
 
+        assert gain is not None
         print_verb("")
         print_verb(self.dual_problem.get_objective_fun_name(), gain)
         if self.value is not None:
@@ -715,9 +716,9 @@ class ConjugateGradientDescentSolver(GradientDescentSolver):
 
         if self.use_linesearch:
             self.step_size, gain = self.get_step_size_ls(gain)
-
-        self.current_guess = self.current_guess + self.step_size * self.grad
-        self.current_guess = self.normalize_field(self.current_guess)
+        else:
+            self.current_guess = self.current_guess + self.step_size * self.grad
+            self.current_guess = self.normalize_field(self.current_guess)
         if self.dual_problem.forward_equation.constant_mass_flux:
             self.current_guess = (
                 self.dual_problem.forward_equation.update_velocity_field(
