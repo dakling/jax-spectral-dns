6ce8de7e3f0b7a368ab22b2cc9af543e434f1758
Dario <dario.klingenberg@web.de>
2024-09-26 10:17:04 +0100

    cleanup

--- jax_spectral_dns/gradient_descent_solver.py
+++ jax_spectral_dns/gradient_descent_solver.py
@@ -715,12 +715,6 @@ class ConjugateGradientDescentSolver(GradientDescentSolver):
 
         if self.use_linesearch:
             self.step_size, gain = self.get_step_size_ls(gain)
-            if self.value is not None:
-                print_verb(
-                    self.dual_problem.get_objective_fun_name(),
-                    "change:",
-                    (gain - self.value),
-                )
         else:
             self.current_guess = self.current_guess + self.step_size * self.grad
             self.current_guess = self.normalize_field(self.current_guess)
