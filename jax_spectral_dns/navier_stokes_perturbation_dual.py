77e49ff26d6d357f6b1565648c7712c019ea2206
Dario <dario.klingenberg@web.de>
2025-03-31 14:13:27 +0200

    revert

--- jax_spectral_dns/navier_stokes_perturbation_dual.py
+++ jax_spectral_dns/navier_stokes_perturbation_dual.py
@@ -1162,33 +1162,34 @@ class NavierStokesVelVortPerturbationDual(NavierStokesVelVortPerturbation):
         c_0 = v_0.energy_with_other(u_0)
         A = (step_size * c_0 - 2 * e_0) / (2 * step_size * e_0)  # TODO plus/minus?
         lam = A + jnp.sqrt(A**2 - (step_size * e_0_adj - c_0) / (step_size * e_0))
-        # lam = -1.0
+        print(lam)
+        lam = -1.0
 
-        # def get_new_energy_0(l: float) -> float:
-        #     return (
-        #         (
-        #             (1.0 + step_size * l) * u_hat_0
-        #             + step_size / self.gain * (-1 * v_hat_0 + beta * old_grad)
-        #         )
-        #         .no_hat()
-        #         .energy()
-        #     )
+        def get_new_energy_0(l: float) -> float:
+            return (
+                (
+                    (1.0 + step_size * l) * u_hat_0
+                    + step_size / self.gain * (-1 * v_hat_0 + beta * old_grad)
+                )
+                .no_hat()
+                .energy()
+            )
 
-        # print_verb("optimising lambda...", verbosity_level=2)
-        # i = 0
-        # max_iter = 100
-        # tol = 1e-25  # can be fairly high as we normalize the result anyway
-        # while abs(get_new_energy_0(lam) - e_0) / e_0 > tol and i < max_iter:
-        #     lam += -(get_new_energy_0(lam) - e_0) / jax.grad(get_new_energy_0)(lam)
-        #     i += 1
-        # print_verb(
-        #     "optimising lambda done in",
-        #     i,
-        #     "iterations, lambda:",
-        #     lam,
-        #     verbosity_level=2,
-        # )
-        # print_verb("energy:", get_new_energy_0(lam), verbosity_level=2)
+        print_verb("optimising lambda...", verbosity_level=2)
+        i = 0
+        max_iter = 100
+        tol = 1e-25  # can be fairly high as we normalize the result anyway
+        while abs(get_new_energy_0(lam) - e_0) / e_0 > tol and i < max_iter:
+            lam += -(get_new_energy_0(lam) - e_0) / jax.grad(get_new_energy_0)(lam)
+            i += 1
+        print_verb(
+            "optimising lambda done in",
+            i,
+            "iterations, lambda:",
+            lam,
+            verbosity_level=2,
+        )
+        print_verb("energy:", get_new_energy_0(lam), verbosity_level=2)
 
         return (
             (
