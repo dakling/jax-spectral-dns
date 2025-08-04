ed2fa3a63369557c14056ec0d70ccba5e0d57528
Dario <dario.klingenberg@web.de>
2025-08-04 13:09:13 +0200

    debug

--- jax_spectral_dns/examples.py
+++ jax_spectral_dns/examples.py
@@ -3601,6 +3601,8 @@ def run_secondary_growth(**params: Any) -> None:
     v0_hat.set_name("velocity_hat")
 
     v_total = v0_hat.no_hat() + vel_base_hat.no_hat()
+    print("vtotal")
+    print([v_total[i].max() for i in range(3)])
     dt = Equation.find_suitable_dt(
         domain, max_cfl, tuple([v_total[i].max() for i in range(3)]), end_time__
     )
